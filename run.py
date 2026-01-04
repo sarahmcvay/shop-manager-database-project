033678a83c8d37cb9210fa0e037b4f1acfd69b2b
sarahmcvay <tosarah.s@gmail.com>
2026-01-04 13:59:01 +0000

    Added functionailty to display orders with items in terminal interface

--- run.py
+++ run.py
@@ -9,7 +9,7 @@
 
 from lib.database_connection import DatabaseConnection
 from lib.item_repository import ItemRepository
-from lib.order_repository import *
+from lib.order_repository import OrderRepository
 
 class Application():
     def __init__(self):
@@ -21,19 +21,49 @@ class Application():
         print("Welcome to the muppety boutique inventory system")
         print("what would you like to do?")
         print("1 - List all items in stock")
-        print("2 - Add item to stock")
+        print("2 - Create new item in shop")
         print("3 - List all orders")
-        print("4 - Add new order")
+        print("4 - List all orders with corresponding items")
+        print("5 - Create new order")
         choice = input("Enter your choice:  ")
     
-        if choice == "1":
+        if choice == "1": 
             item_repository = ItemRepository(self._connection)
             items = item_repository.all()
             for item in items:
                 print (f"{item.descript}: price £{item.price}, stock {item.quantity}")
+
+        if choice == "3":
+            order_repository = OrderRepository(self._connection)
+            orders = order_repository.all()
+            for order in orders:
+                print(f"Customer: {order.customer}, Date: {order.date}")
+
+        if choice == "4":
+            order_repository = OrderRepository(self._connection)
+            orders = order_repository.all_orders_with_items()
+            for order in orders:
+                print(f"Customer: {order['customer']}, Date: {order['date']}")
+                for item in order["items"]:
+                    print(
+                        f"  - {item['descript']}"
+                        f"(qty: {item['quantity']}, price: £{item['price']})"
+                    )
+                # print(order)
+
+# can I print items in the order? 
+
+        # elif choice == "3":
+        #     print("Functionality coming soon!")
+
+        # elif choice == "4":
+        #     print("Functionality coming soon!")
         
+        # elif choice == "5":
+        #     print("Functionality coming soon!")
+
         else: 
-            return print("Functionality coming soon!")
+            return print("Please try again, invalid entry.")
 
 if __name__ == "__main__":
     app = Application()
