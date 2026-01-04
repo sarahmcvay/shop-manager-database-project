b4bbd04989c77d804ce2725bc93de4dcd4ad4d0f
sarahmcvay <tosarah.s@gmail.com>
2026-01-04 14:02:47 +0000

    added method for accessing orders with items

--- lib/order_repository.py
+++ lib/order_repository.py
@@ -32,9 +32,6 @@ class OrderRepository:
             # str(row["date"])
         )
 
-# def find_by_customer_name(self, order_id):
-    # search by customer name, can find the date of the order. 
-
     def find_items_in_order(self, order_id):
 # allows the user find the items connected to a single order
         rows = self._connection.execute(
@@ -44,9 +41,9 @@ class OrderRepository:
             "FROM orders "
             # link tables together
             # join table first, matching records in join table with orders
-                "JOIN items_orders ON items_orders.order_id = orders.id "
+                "JOIN orders_items ON orders_items.order_id = orders.id "
             # then match items to records in join table 
-                "JOIN items ON items_orders.item_id = items.id " \
+                "JOIN items ON orders_items.item_id = items.id " \
                 "WHERE orders.id = %s", [order_id]
         )
         itemsinorder = []
@@ -60,3 +57,44 @@ class OrderRepository:
         
         return itemsinorder
 
+        # see shop manager notes for thinking.
+
+    def all_orders_with_items(self):
+        rows = self._connection.execute(
+            "SELECT o.id AS order_id, o.customer, o.date, "
+            "oi.quantity AS order_quantity, i.descript, i.price "
+            "FROM orders o "
+            "JOIN orders_items oi ON oi.order_id = o.id "
+            "JOIN items i ON i.id = oi.item_id "
+            "ORDER BY o.id, i.descript "
+        )
+        orders_dict = {} # dict allows us to group rows by order_id
+        for row in rows: 
+            order_id = row["order_id"]
+        
+        # if we haven't seen the order yet, create an entry in the dict
+            if order_id not in orders_dict: 
+                orders_dict[order_id] = {
+                    "id": order_id,
+                    "customer": row["customer"],
+                    "date": row["date"],
+                    "items": []
+                }
+
+        # add the item (if there is one)
+            if row["descript"] is not None:
+                orders_dict[order_id]["items"].append({
+                    "descript": row["descript"],
+                    "quantity": row["order_quantity"],
+                    "price": row["price"]
+                    })
+
+        return list(orders_dict.values())
+
+
+
+    # def remove_items_in_an_order_from_stock(self):
+    #     pass 
+
+# def find_by_customer_name(self, order_id):
+    # search by customer name, can find the date of the order. 
